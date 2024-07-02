42e4942194f72a1ac05ba3fa71e7f702d2122189
aro <158510552+aromeye@users.noreply.github.com>
2024-07-02 16:56:08 -0400

    Update pong.py
    *B* *A* *L* *L* movement

--- pong.py
+++ pong.py
@@ -1,12 +1,11 @@
 from pygame import *
 from random import randint
-from time import *
 
 init()
 
 SCREEN_WIDTH, SCREEN_HEIGHT= 800, 600
 SPRITE_WIDTH, SPRITE_HEIGHT = 65, 65
-background = (0, 0, 0)
+background = (0, 0, 255)
 pelota_img = 'ball.png'
 FPS = 60
 
@@ -15,27 +14,50 @@ display.set_caption('pelota')
 
 class GameSprite(sprite.Sprite):
     # constructor de clase
-    def __init__(self, pos_image, pos_x, pos_y, size_x, size_y):
+    def __init__(self, pos_image, pos_x, pos_y, size_x, size_y, speed):
         # llamamos al constructor de la clase (Sprite):
         sprite.Sprite.__init__(self)
         # cada objeto debe almacenar una propiedad image
         self.image = transform.scale(image.load(pos_image), (size_x, size_y))
         # cada objeto debe almacenar la propiedad rect en la cual está inscrito
+        self.widht = size_x
+        self.height = size_y
         self.rect = self.image.get_rect()
         self.rect.x = pos_x
         self.rect.y = pos_y
+        self.speed = speed
+    
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
 
 class Pelota(GameSprite):
+    def __init__(self, pos_image, pos_x, pos_y, size_x, size_y, speed_y, speed_x, speed=0):
+        # llamamos al constructor de la clase (Sprite):
+        super().__init__(pos_image, pos_x, pos_y, size_x, size_y, speed)
+        self.speed_x = speed_x
+        self.speed_y = speed_y
+        
     def update(self):
-        self.rect.y += self.speed
-        if self.rect.y >= SCREEN_HEIGHT:
-            self.rect.y = 0
-            self.rect.x = randint(0, SCREEN_WIDTH - 50)
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+        if self.rect.y >= SCREEN_HEIGHT -self.height or self.rect.y <= 0:
+            self.speed_y *= -1 
 
 
-pelota = Pelota(pelota_img, 400, 300, 50, 50)
 
-#clock = time.Clock()
+class Wall1(GameSprite):
+    def move(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.x > 5:
+           self.rect.y -= self.speed
+        if keys[K_s] and self.rect.x < 635:
+           self.rect.y += self.speed
+
+pelota = Pelota(pelota_img, 400, 300, 50, 50, 5, 5)
+wall = Wall1(pelota_img, 780, 175, 10, 300, 0)
+
+clock = time.Clock()
 finish = False
 run = True
 
@@ -45,8 +67,13 @@ while run:
             run = False
     if not finish:
         window.fill(background)
+        pelota.reset()
+        pelota.update()
+        wall.reset()
+        wall.move()
+        
 
     display.update()
-    #clock.tick(FPS)
+    clock.tick(FPS)
 
 quit()
